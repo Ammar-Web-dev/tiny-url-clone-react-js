2484d0e5b892e95f83df6dd907e018694a596b4e
Muhammad Ammar <legendammar04@gmail.com>
2026-07-06 01:25:08 +0500

    tiny-url-frontend-clone

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,54 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router";
+import { ClerkProvider } from "@clerk/clerk-react";
 
-createRoot(document.getElementById('root')).render(
+import "./index.css";
+
+import Dashboard from "./pages/Dashboard";
+import Home from "./pages/Home";
+import Login from "./pages/Login.jsx";
+import About from "./pages/About";
+import Blog from "./pages/Blog";
+import Contact from "./pages/Contact";
+import HelpCenter from "./pages/HelpCenter";
+import Privacy from "./pages/Privacy";
+import Terms from "./pages/Terms";
+import Analytics from "./pages/Analytics";
+import Pricing from "./pages/Pricing";
+import Features from "./pages/Features.jsx";
+import Footer from "./components/Footer";
+import FAQ from "./pages/FAQ";
+import NavFeatures from "./pages/NavFeatures.jsx"
+import NavPricing from "./pages/NavPricing.jsx"
+
+const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
+
+if (!PUBLISHABLE_KEY) {
+  throw new Error("Missing Clerk Publishable Key");
+}
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
+    <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/home" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/blog" element={<Blog />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/help-center" element={<HelpCenter />} />
+          <Route path="/privacy" element={<Privacy />} />
+          <Route path="/terms" element={<Terms />} />
+          <Route path="/analytics" element={<Analytics />} />
+          <Route path="/pricing" element={<Pricing />} />
+          <Route path="/features" element={<Features />} />
+          {/* Navbarr */}
+          <Route path="/faq" element={<FAQ />} />
+          <Route path="/features" element={<NavFeatures />} />
+          <Route path="/pricing" element={<NavPricing />} />
+        </Routes>
+      </BrowserRouter>
+    </ClerkProvider>
   </StrictMode>,
-)
+);
